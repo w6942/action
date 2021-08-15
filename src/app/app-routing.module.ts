a8f29bce0ccb686d45961790711f086e8cd3a6fa
w6942 <w694287414@qq.com>
2021-08-15 23:22:01 +0800

    fix login style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: '**', redirectTo: 'login' }
 ];
